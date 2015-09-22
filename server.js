3010760b04d75cad5bbacfb827ac580b5d511966
pkn <pavannelakuditi@gmail.com>
2015-09-21 22:58:15 -0400

    express serving index.html on port 3001

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app= express();
+
+app.use(express.static('app'));
+app.get('/',function(req,res){
+    res.sendfile(__dirname + '/app/index.html');
+})
+ var server=app.listen(3001, function(){
+     var host = server.address().address;
+     var port = server.address().port;
+
+     console.log('Listening at http://localhost:%d', port);
+})
