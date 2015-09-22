0256360748385a0ba3145b47fdac9c058ddb49f8
pkn <pavannelakuditi@gmail.com>
2015-09-22 01:00:30 -0400

    ui-router and angular are added

--- app/app.js
+++ app/app.js
@@ -1,3 +1,19 @@
 /**
  * Created by pkn on 9/21/15.
  */
+(function(){
+    "use strict";
+    angular.module('myApp',["ui.router"])
+        .config([ '$stateProvider', '$urlRouterProvider', function( $stateProvider, $urlRouterProvider) {
+
+            $urlRouterProvider.otherwise("/main");
+
+            $stateProvider
+                .state("base", {
+                    url : "/main" ,
+                    templateUrl : "partials/base.html",
+                    controller : "baseController as bc"
+                })
+
+        }])
+})()
---
+++ app/partials/baseCtrl.js
@@ -0,0 +1,8 @@
+/**
+ * Created by pkn on 9/22/15.
+ */
+angular.module('myApp').controller("baseController",function(){
+    var bc=this;
+    bc.name="BASE";
+
+})
